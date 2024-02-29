6fbb6023a745ab3d90b4c0b399a1ef6e53d09e0a
MusaddikTahmid <musaddiktahmid@gmail.com>
2024-02-29 08:08:02 +0600

    loading added

--- js/phone.js
+++ js/phone.js
@@ -16,6 +16,17 @@ const displayPhones = (phones) => {
   //   clear phone container card before adding new card
   phoneContainer.textContent = "";
 
+  // display show all button if there are more than 12 phones
+  const showAllContainer = document.getElementById("show-all-container");
+  if (phones.length > 12) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
+  }
+
+  // display only first 12 phones
+  phones = phones.slice(0, 12);
+
   phones.forEach((phone) => {
     console.log(phone);
     // 2: create a div
@@ -29,26 +40,45 @@ const displayPhones = (phones) => {
                 alt="Shoes"
               />
             </figure>
-    <div class="card-body">
+    <div class="card-body text-center">
         <h2 class="card-title">${phone.phone_name}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p>There are many variations of passages of available, but the majority have suffered</p>
         <div class="card-actions justify-center">
-        <button class="btn btn-primary">Buy Now</button>
+        <button class="btn btn-primary">Show Details</button>
         </div>
     </div>
     `;
     // append child
     phoneContainer.appendChild(phoneCard);
   });
+  // hide loading spinner
+  toggleLoadingSpinner(false);
 };
 
 // handle search button
 
 const handleSearch = () => {
+  toggleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   console.log(searchText);
   loadPhone(searchText);
+};
+// handle search recap
+// const handleSearch2 = () => {
+//   toggleLoadingSpinner(true);
+//   const searchField = document.getElementById("search-field2");
+//   const searchText = searchField.value;
+//   loadPhone(searchText);
+// };
+
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
 };
 
 // loadPhone();
